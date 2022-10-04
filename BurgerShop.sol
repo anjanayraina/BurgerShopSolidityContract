6becfea9b91e554645327c63110c59585f48c569
Anjanay Raina <anjanayraina326@gmail.com>
2022-10-04 09:56:28 +0530

    Conrtact Base Created

--- BurgerShop.sol
+++ BurgerShop.sol
@@ -2,14 +2,38 @@ pragma solidity ^0.8.0;
 // SPDX-License-Identifier: GPL-3.0
 contract BurgerShop{
     Burger[] menu;
+    address payable public   owner;
+    enum Stages {
+            readyToOrder,
+            cooking,
+            readyToTake
+
+        }
     struct Burger{
         uint cost;
         uint totalSupply;
         string name;
         uint currentSupply;
+        Stages currentStage;
         
 
     }
+
+    constructor(address payable _owner){
+        owner = _owner;
+    }
+
+    event boughtBurger(address indexed from  , Burger burger);
+
+    modifier isOwner() {
+
+        require(msg.sender == owner);
+        _;
+    }
+
+    
+
+    
 
 
 }
